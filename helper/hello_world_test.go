795c7f9836b371f85a6071914c265b53fc7846a3
Zakaria Wahyu <zakarianur6@gmail.com>
2021-02-27 02:10:51 +0700

    Mengagalkan unit test, Assertion & Require, Skip Test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,7 +1,108 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"runtime"
+	"testing"
+)
 
+/**
+Menggagalkan Unit Test
+- Menggagalkan unit test menggunakan panic bukanlah hal yang bagus.
+- Golang sendiri sudah menyediakan cara untuk menggagalkan unit test menggunakan testing.T
+- Terdapat function Fail(), FailNow(), Error(), Fatal()
+ */
+
+/**
+t.Fail() dan t.FailNow()
+- Terdapat dua fintion untuk menggagalkan unit test, yaitu Fail() dan  FailNow()
+- Fail() akan menggagalkan unit test, namun akan tetap melanjutkan ekseskusi unit test, tapi diakhir ketika selesai maka unit test tersebut dianggal gagal
+- FailNow() akan menggagalkan unit test saat itu juga tanpa melanjutkan eksekusi unit test atau dipaksa untuk berhenti
+ */
+
+/**
+t.Error(args...) dan t.Fatal(args...)
+- Error() function lebih seperti melakukan log print ketika error, namun setelah melakukan log eorror, maka akan secara otomatis memanggil function Fail(), artinya eksekusi unit test akan tetap berjalan sampai selesai
+- Fatal() mirip dengan Error(), hanya saja setelah melakukan log error akan memanggil funtion FailNow() , sehingga dipaksa berhenti unit testingnya
+ */
+
+/**
+Assertion
+- Melakukan pengecekan di unit test secara manual menggunakan if else sangatlah menyebalkan apalagi result sata yang di cek itu banyak
+- Oleh karena itu disarankan menggunakan assertion untuk melakukan pengecekan
+- Sayangnya di golang tidak menyediakan package untuk assertion, sehingga kita butuh menambahkan library untuk melakukan assertion
+
+Testify
+-Salah satu library yang paling populer di golang adalah Testify
+-Kita bisa menggunakan library ini untuk melakukan assertion terhadap result data di unit test
+- https://github.com/stretchr/testify
+
+assert vs require
+- Saat kita menggunakan assert, jika pengecekan gagal maka assert akan memanggil Fail() artinya eksekusi unit test teap dilanjutkan
+- Sedangkan jika kita menggunakan require, jika pengecekan gagal maka  akan memanggil FailNow() artinya dipaksa berhenti dan eksekusi unit test tidak akan dilanjutkan
+ */
+
+/**
+Skip Test
+- Kadang dalam kondisi tertentu , kita ingin membatalkan eksekusi unit test
+- Di golang juga kita bisa membatalkan eksekusinya jika kita mau
+- Untuk membatalkan atau skip unit test bisa menggunakan fucntion Skip()
+ */
+
+// ketika fail maka akan tetap lanjut tapi diakhir dianggap gagal
+func TestHelloZakaria(t *testing.T) {
+	result := HelloWorld("Zakaria")
+
+	if result != "Hello Zakaria Test.." {
+		// menggagalkan unit test
+		t.Fail()
+	}
+
+	fmt.Println("Test Hello Zakaria Done....")
+}
+
+
+// ketika failnow akan dipaksa berhenti dan tidak melanjutkan unit test
+func TestHelloWahyu(t *testing.T) {
+	result := HelloWorld("Wahyu")
+
+	if result != "Hello Wahyu Test.." {
+		// menggagalkan unit test
+		t.FailNow()
+	}
+
+	fmt.Println("Test Hello Wahyu Done....")
+}
+
+// ketika Error() maka bisa mencetak pesan error
+// kemudian otomatis memanggil Fail(). akan tetap lanjut tapi diakhir dianggap gagal
+func TestHelloNur(t *testing.T) {
+	result := HelloWorld("Nur")
+
+	if result != "Hello Nur Test.." {
+		// menggagalkan unit test
+		t.Error("This result not Hello Nur")
+	}
+
+	fmt.Println("Test Hello Nur Done....")
+}
+
+// ketika Fatal() maka bisa mencetak pesan error
+// kemudian otomatis memanggil FailNow(). akan dipaksa berhenti dan tidak melanjutkan unit test
+func TestHelloUtomo(t *testing.T) {
+	result := HelloWorld("Utomo")
+
+	if result != "Hello Utomo Test.." {
+		// menggagalkan unit test
+		t.Fatal("This result not Hello Utomo")
+	}
+
+	fmt.Println("Test Hello Utomo Done....")
+}
+
+// Contoh unit test berhasil
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Zakaria")
 
@@ -11,6 +112,8 @@ func TestHelloWorld(t *testing.T) {
 	}
 }
 
+// sengaja di errorkan dan menggunakan panic
+// tidak disarankan menggagalkan error dengan panic
 func TestHelloWorlZakaria(t *testing.T) {
 	result := HelloWorld("Zakaria")
 
@@ -19,3 +122,33 @@ func TestHelloWorlZakaria(t *testing.T) {
 		panic("Result is not Hello Zakaria")
 	}
 }
+
+// pembandung menggunakan assert
+// Jika gagal maka akan memanggil Fail()
+func TestHelloWorldAssert(t *testing.T)  {
+	result := HelloWorld("Zakaria")
+	assert.Equal(t, "Hello Zakaria Cek...", result, "Results must be 'Hello Zakaria Cek...'")
+	fmt.Println("Test selesai")
+}
+
+// pembandung menggunakan require
+// Jika gagal maka akan memanggil FailNow()
+func TestHelloWorldRequire(t *testing.T)  {
+	result := HelloWorld("Zakaria")
+	require.Equal(t, "Hello Zakaria Cek...", result, "Results must be 'Hello Zakaria Cek...'")
+	fmt.Println("Test selesai")
+}
+
+// Biasanya ada kondisi diawal untuk menanyakan apakah perlu di skip atau tidak
+// Jika sudah sampai funstion Skip() maka kode program unit test dibawahnya tidak akan dijalankan
+func TestHelloWorldSkip(t *testing.T)  {
+	if runtime.GOOS == "windows" {
+		t.Skip("Unit test can't run in operating system windows")
+	}
+	result := HelloWorld("Zakaria")
+	require.Equal(t, "Hello Zakaria Cek...", result, "Results must be 'Hello Zakaria Cek...'")
+	fmt.Println("Test selesai")
+}
+
+
+
